51e13efa44c41a17296ff440ad401ece119a76dc
Miguel Rosales <miiguelangelrv@gmail.com>
2024-05-08 17:06:19 -0600

    new gym

--- departamentoMR/gimnasio.py
+++ departamentoMR/gimnasio.py
@@ -1,208 +1,111 @@
 from customtkinter import *
-from tkinter import messagebox
-
-from componentes_graficos.LtkButton import LtkButtonFill, LtkButtonLine
+from tkinter import messagebox, simpledialog
+from componentes_graficos.LtkButton import LtkButtonFill, LtkButtonLine, LtkButtonTransparentBackground
 from componentes_graficos.LtkEntry import LtkEntryLine, LtkEntryFill
 from componentes_graficos.LtkLabel import LtkLabel
+from componentes_graficos.LtkCheckBox import LtkCheckBoxFill
 from componentes_graficos.LtkComboBox import LtkComboBoxLine
 from componentes_graficos.LtkTreeView import LtkFileInputTreeView
-
 import json
 import os
 
 class Gimnasio:
     def __init__(self):
-        ventana = CTk()
-        ventana.title("Configuracion Gimnasio")
-        ventana.geometry("1200x800+350+100")
-        ventana.configure(bg="#FFFFFF")
+        self.ventana=CTk()
+        self.ventana.title("Configuracion Gimnasio")
+        self.ventana.geometry("800x500+350+100")
+        self.ventana.configure(bg="#FFFFFF")
+
+        self.ruta_ventana=os.path.dirname(os.path.abspath(__file__))
+        
+        # Crear variables para almacenar datos
+        self.cantidad_empleados=IntVar()
+
+
 
         #Frame para titulo
-        frame_titulo = CTkFrame(ventana)
+        frame_titulo=CTkFrame(self.ventana)
         frame_titulo.pack(fill=BOTH, expand=True)
-        titulo = LtkLabel(frame_titulo, "Configuracion Gimnasio")
+        titulo=LtkLabel(frame_titulo, "Configuracion Gimnasio")
         titulo.configure(font=("Poppins", 40, "bold"))
         titulo.pack(side=TOP, padx=20, pady=20)
 
 
-        #Frame para personalGYM
-        frame_personalGYM = CTkFrame(ventana)
-        frame_personalGYM.pack(fill=BOTH, expand=True)
-        boton_personalGYM = LtkButtonFill(frame_personalGYM, self.personalgym, "Personal del GYM")
-        boton_personalGYM.pack(side=LEFT, padx=70, pady=20)
-        
-        #Frame costo
-        frame_costo = CTkFrame(ventana)
-        frame_costo.pack(fill=BOTH, expand=True)
-        boton_costos = LtkButtonFill(frame_costo, self.costos, "Costos")
-        boton_costos.pack(side=LEFT, padx=70, pady=10)
+        #Frame prinicial
+        frame_prinicial=CTkFrame(self.ventana)
+        frame_prinicial.pack(fill=BOTH, expand=True)
 
-        #Frame para horarios
-        frame_horarios = CTkFrame(ventana)
-        frame_horarios.pack(fill=BOTH, expand=True)
-        boton_horarios = LtkButtonFill(frame_horarios, self.horarios, "Horarios")
-        boton_horarios.pack(side=LEFT, padx=70, pady=10)
+        boton_prinicial=LtkButtonLine(frame_prinicial, self.personal, "Personal del GYM")
+        boton_prinicial.grid(row=0, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_costos=LtkButtonLine(frame_prinicial, self.costos, "Costos")
+        boton_costos.grid(row=1, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_horarios=LtkButtonLine(frame_prinicial, self.horarios, "Horarios")
+        boton_horarios.grid(row=2, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_usuarios=LtkButtonLine(frame_prinicial, self.usuarios, "Usuarios")
+        boton_usuarios.grid(row=3, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_maquinas=LtkButtonLine(frame_prinicial, self.maquinas, "Maquinas")
+        boton_maquinas.grid(row=4, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_servicios_generales=LtkButtonLine(frame_prinicial, self.servicios_generales, "Servicios Generales")
+        boton_servicios_generales.grid(row=5, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_baños=LtkButtonLine(frame_prinicial, self.baños, "Baños")
+        boton_baños.grid(row=6, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_vestidores=LtkButtonLine(frame_prinicial, self.vestidores, "Vestidores")
+        boton_vestidores.grid(row=7, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
+        boton_temporadas=LtkButtonLine(frame_prinicial, self.temporadas, "Temporadas")
+        boton_temporadas.grid(row=8, column=0, padx=(5,5), pady=(5, 5), sticky="nsew")
 
-        #Frame para usuarios
-        frame_usuarios = CTkFrame(ventana)
-        frame_usuarios.pack(fill=BOTH, expand=True)
-        boton_usuarios = LtkButtonFill(frame_usuarios, self.usuarios, "Usuarios")
-        boton_usuarios.pack(side=LEFT, padx=70, pady=10)
 
-        #Frame para maquinas
-        frame_maquinas = CTkFrame(ventana)
-        frame_maquinas.pack(fill=BOTH, expand=True)
-        boton_maquinas = LtkButtonFill(frame_maquinas, self.maquinas, "Maquinas")
-        boton_maquinas.pack(side=LEFT, padx=70, pady=10)
-
-        #Frame para servicio general
-        frame_serviciogeneral = CTkFrame(ventana)
-        frame_serviciogeneral.pack(fill=BOTH, expand=True)
-        boton_serviciogeneral = LtkButtonFill(frame_serviciogeneral, self.serviciogeneral, "Servicio General")
-        boton_serviciogeneral.pack(side=LEFT, padx=70, pady=10)
-
-        #Frame para baños
-        frame_baños = CTkFrame(ventana)
-        frame_baños.pack(fill=BOTH, expand=True)
-        boton_baños = LtkButtonFill(frame_baños, self.baños, "Baños")
-        boton_baños.pack(side=LEFT, padx=70, pady=10)
-
-        #Frame para vestidores
-        frame_vestidores = CTkFrame(ventana)
-        frame_vestidores.pack(fill=BOTH, expand=True)
-        boton_vestidores = LtkButtonFill(frame_vestidores, self.vestidores, "Vestidores")
-        boton_vestidores.pack(side=LEFT, padx=70, pady=10)
-
-        #Frame para temporadas
-        frame_temporadas = CTkFrame(ventana)
-        frame_temporadas.pack(fill=BOTH, expand=True)
-        boton_temporadas = LtkButtonFill(frame_temporadas, self.temporadas, "Temporadas")
-        boton_temporadas.pack(side=LEFT, padx=70, pady=10)
-
-        #Frame para salir configuracion
-        frame_guardar = CTkFrame(ventana)
+        #Frame para guardar
+        frame_guardar=CTkFrame(self.ventana)
         frame_guardar.pack(fill=BOTH, expand=True)
-        boton_guardar = LtkButtonFill(frame_guardar, self.salir, "Salir de Configuracion Gimnasio")
+        boton_guardar = LtkButtonFill(frame_guardar, self.guardar_informacion, "Guardar Configuracion Banco")
         boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
         
-        ventana.mainloop()
+        self.ventana.mainloop()
+        
+    def resetear_frame_caracteristicas(self):
+        for widget in self.frame_caracteristicas.winfo_children():
+            widget.destroy()
 
-    def salir(self):
-        messagebox.showinfo("SALIENDO", "Configuracion Guardada")
-        exit()
+    def guardar_informacion(self):
+        informacion={
+            "cantidad_empleados": self.cantidad_empleados.get()
+            # Agregar más campos según los datos recolectados
+        }
+        
+        informacion_json=json.dumps(informacion, indent=4)
+        config_path=os.path.join(self.ruta_ventana, 'gimnasio.json')
 
+        with open(config_path, 'w') as f:
+            f.write(informacion_json)
 
-
-    def personalgym(self):
-        self.ajustes_personal = CTk()
-        self.ajustes_personal.title("Ajustes Personal del GYM")
-        self.ajustes_personal.geometry("800x600+660+210")
-        self.ajustes_personal.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_personal, self.guardar_personal, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_personal.mainloop()
-    def guardar_personal(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_personal.destroy()
-
-
-    def costos(self):
-        self.ajustes_costos = CTk()
-        self.ajustes_costos.title("Ajustes Costos")
-        self.ajustes_costos.geometry("800x600+660+210")
-        self.ajustes_costos.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_costos, self.guardar_costos, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_costos.mainloop()
-    def guardar_costos(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_costos.destroy()
-
-
-
-    def horarios(self):
-        self.ajustes_horarios = CTk()
-        self.ajustes_horarios.title("Ajustes Horarios")
-        self.ajustes_horarios.geometry("800x600+660+210")
-        self.ajustes_horarios.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_horarios, self.guardar_horarios, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_horarios.mainloop()
-    def guardar_horarios(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_horarios.destroy()
-
-
-    def usuarios(self):
-        self.ajustes_usuarios = CTk()
-        self.ajustes_usuarios.title("Ajustes Usuarios")
-        self.ajustes_usuarios.geometry("800x600+660+210")
-        self.ajustes_usuarios.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_usuarios, self.guardar_usuarios, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_usuarios.mainloop()
-    def guardar_usuarios(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_usuarios.destroy()
+        messagebox.showinfo("Informacion", "Informacion guardada correctamente")
+        self.ventana.destroy()
 
     
+
+    def personal(self):
+        cantidad_empleados=simpledialog.askinteger("Cantidad de Empleados", "Ingrese la cantidad de empleados:")
+        if cantidad_empleados is not None:
+            self.cantidad_empleados.set(cantidad_empleados)
+
+    def costos(self):
+        pass
+    def horarios(self):
+        pass
+    def usuarios(self):
+        pass
     def maquinas(self):
-        self.ajustes_maquinas = CTk()
-        self.ajustes_maquinas.title("Ajustes Maquinas")
-        self.ajustes_maquinas.geometry("800x600+660+210")
-        self.ajustes_maquinas.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_maquinas, self.guardar_maquinas, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_maquinas.mainloop()
-    def guardar_maquinas(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_maquinas.destroy()
-
-    def serviciogeneral(self):
-        self.ajustes_serviciogeneral = CTk()
-        self.ajustes_serviciogeneral.title("Ajustes Servicio General")
-        self.ajustes_serviciogeneral.geometry("800x600+660+210")
-        self.ajustes_serviciogeneral.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_serviciogeneral, self.guardar_serviciogeneral, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_serviciogeneral.mainloop()
-    def guardar_serviciogeneral(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_serviciogeneral.destroy()
-
-
+        pass
+    def servicios_generales(self):
+        pass
     def baños(self):
-        self.ajustes_baños = CTk()
-        self.ajustes_baños.title("Ajustes Baños")
-        self.ajustes_baños.geometry("800x600+660+210")
-        self.ajustes_baños.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_baños, self.guardar_baños, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_baños.mainloop()
-    def guardar_baños(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_baños.destroy()
-
+        pass
     def vestidores(self):
-        self.ajustes_vestidores = CTk()
-        self.ajustes_vestidores.title("Ajustes Vestidores")
-        self.ajustes_vestidores.geometry("800x600+660+210")
-        self.ajustes_vestidores.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_vestidores, self.guardar_vestidores, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_vestidores.mainloop()
-    def guardar_vestidores(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_vestidores.destroy()
-
+        pass
     def temporadas(self):
-        self.ajustes_temporadas = CTk()
-        self.ajustes_temporadas.title("Ajustes Temporadas")
-        self.ajustes_temporadas.geometry("800x600+660+210")
-        self.ajustes_temporadas.configure(bg="#FFFFFF")
-        boton_guardar = LtkButtonFill(self.ajustes_temporadas, self.guardar_temporadas, "Guardar Y Salir De Ajustes")
-        boton_guardar.pack(side=BOTTOM, padx=20, pady=20)
-        self.ajustes_temporadas.mainloop()
-    def guardar_temporadas(self):
-        messagebox.showinfo("GUARDADO", "Ajustes Guardados")
-        self.ajustes_temporadas.destroy()
+        pass
+
+Gimnasio()
+      
+
